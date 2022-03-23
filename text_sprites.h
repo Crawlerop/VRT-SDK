55964788a5357b79d2171bad80a5d4e04f20bf0d
UserDeploy <thisisnotpremium@gmail.com>
2022-03-23 20:42:51 +0700

    first commit

---
+++ text_sprites.h
@@ -0,0 +1,108 @@
+const unsigned char LETTER_0[] = {
+0x0, 0x10
+};
+const unsigned char LETTER_1[] = {
+0x1, 0x11
+};
+const unsigned char LETTER_2[] = {
+0x2, 0x12
+};
+const unsigned char LETTER_3[] = {
+0x3, 0x13
+};
+const unsigned char LETTER_4[] = {
+0x4, 0x14
+};
+const unsigned char LETTER_5[] = {
+0x5, 0x15
+};
+const unsigned char LETTER_6[] = {
+0x6, 0x16
+};
+const unsigned char LETTER_7[] = {
+0x7, 0x17
+};
+const unsigned char LETTER_8[] = {
+0x8, 0x18
+};
+const unsigned char LETTER_9[] = {
+0x9, 0x19
+};
+const unsigned char LETTER_a[] = {
+0xa, 0x1a
+};
+const unsigned char LETTER_b[] = {
+0xb, 0x1b
+};
+const unsigned char LETTER_c[] = {
+0xc, 0x1c
+};
+const unsigned char LETTER_d[] = {
+0xd, 0x1d
+};
+const unsigned char LETTER_e[] = {
+0xe, 0x1e
+};
+const unsigned char LETTER_f[] = {
+0xf, 0x1f
+};
+const unsigned char LETTER_10[] = {
+0x20, 0x30
+};
+const unsigned char LETTER_11[] = {
+0x21, 0x31
+};
+const unsigned char LETTER_12[] = {
+0x22, 0x32
+};
+const unsigned char LETTER_13[] = {
+0x23, 0x33
+};
+const unsigned char LETTER_14[] = {
+0x24, 0x34
+};
+const unsigned char LETTER_15[] = {
+0x25, 0x35
+};
+const unsigned char LETTER_16[] = {
+0x26, 0x36
+};
+const unsigned char LETTER_17[] = {
+0x27, 0x37
+};
+const unsigned char LETTER_18[] = {
+0x28, 0x38
+};
+const unsigned char LETTER_19[] = {
+0x29, 0x39
+};
+const unsigned char LETTER_1a[] = {
+0x2a, 0x3a
+};
+const unsigned char LETTER_1b[] = {
+0x2b, 0x3b
+};
+const unsigned char LETTER_1c[] = {
+0x2c, 0x3c
+};
+const unsigned char LETTER_1d[] = {
+0x2d, 0x3d
+};
+const unsigned char LETTER_1e[] = {
+0x2e, 0x3e
+};
+const unsigned char LETTER_1f[] = {
+0x2f, 0x3f
+};
+const unsigned char LETTER_20[] = {
+0x4e, 0x5e
+};
+const unsigned char LETTER_21[] = {
+0x4f, 0x5f
+};
+const unsigned char LETTER_22[] = {
+0x6e, 0x7e
+};
+const unsigned char LETTER_23[] = {
+0x6f, 0x7f
+};
